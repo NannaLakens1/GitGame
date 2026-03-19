8383af9399dae56c2ab201d4543df74fda527a16
NannaLakens1 <n.lakens@student.avans.nl>
2026-03-19 13:12:43 +0100

    Models aangepast aan de aangepaste database

--- GameBackend/Models/Patient.cs
+++ GameBackend/Models/Patient.cs
@@ -9,10 +9,7 @@ namespace GameBackend.Models
         [Required]
         [StringLength(25, MinimumLength = 1)]
         public string? Name { get; set; }
-        [Range(20, 200)]
-        public int? MaxLength { get; set; }
-        [Range(10, 100)]
-        public int? MaxHeight { get; set; }
+        public int? Age { get; set; }
         public string? UserId { get; set; }
     }
 }
